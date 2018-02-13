cd062df00807d9ffdc2eb0b84888dca9d16696b9
Ginel <ginelexaltado@gmail.com>
2018-02-13 15:31:51 +0800

    Users Module & Modified app files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,12 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-
 export const environment = {
-  production: false,
-  firebaseConfig: {
-  	 apiKey: "AIzaSyA0vNKKN0rSEqS-yddAfM_MTvoA5HSiKr8",
-    authDomain: "firestore-crudd.firebaseapp.com",
-    databaseURL: "https://firestore-crudd.firebaseio.com",
-    projectId: "firestore-crudd",
-    storageBucket: "firestore-crudd.appspot.com",
-    messagingSenderId: "47613384541"
-  }
+    production: false,
+    firebaseConfig: {
+    apiKey: "AIzaSyCNsDcXrhXwF4iM5t3RB4fsLh12Yfsq4sQ",
+    authDomain: "midterm-exam-3bdcb.firebaseapp.com",
+    databaseURL: "https://midterm-exam-3bdcb.firebaseio.com",
+    projectId: "midterm-exam-3bdcb",
+    storageBucket: "midterm-exam-3bdcb.appspot.com",
+    messagingSenderId: "834401394828"
+
+    }
 };
